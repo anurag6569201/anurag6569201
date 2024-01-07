3a2839c2ae4e11ad59bce68ff1e7f389d619f02a
anurag singh <anurag6569201@gmail.com>
2024-01-07 10:55:49 +0530

    added reviling csss

--- js/script.js
+++ js/script.js
@@ -122,3 +122,31 @@ window.addEventListener('load', () => {
         loader.style.display = "none";
     },600);
 });
+
+
+// reviling
+gsap.registerPlugin(ScrollTrigger);
+gsap.utils.toArray(".revealUp").forEach(function (elem) {
+  ScrollTrigger.create({
+    trigger: elem,
+    start: "top 70%",
+    end: "bottom 20%",
+    markers: false,
+    onEnter: function () {
+      gsap.fromTo(
+        elem,
+        { y: 100, autoAlpha: 0 },
+        {
+          duration: 1.25,
+          y: 0,
+          autoAlpha: 1,
+          ease: "back",
+          overwrite: "auto"
+        }
+      );
+    },
+    onLeaveBack: function () {
+      gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+    }
+  });
+});
