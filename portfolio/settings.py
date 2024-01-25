c4faa96103189439665a2d58b6f406d4803249b9
anurag singh <anurag6569201@gmail.com>
2024-01-25 12:00:40 +0530

    added cssmin.map

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -125,7 +125,7 @@ STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 
 # renderstorage
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # ......
 
 MEDIA_URL='/media/'
