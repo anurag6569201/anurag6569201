a36ea9b07dd898855bda67ddf5adf67286a587b5
anurag singh <anurag6569201@gmail.com>
2024-03-02 18:27:57 +0530

    added apps

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,14 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path,include
+
 from portfolio import views
 app_name="portfolio"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index,name="index"),
+    path('project/',include('project.urls')),
+    path('time/',include('timeline.urls')),
 ]
 
 if settings.DEBUG:
