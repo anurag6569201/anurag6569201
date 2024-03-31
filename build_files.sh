6e6f999b100c2d5af433c890ecf777dbc87b0e4f
anurag singh <anurag6569201@gmail.com>
2024-03-31 11:42:21 +0530

    up

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,10 @@
+# build_files.sh
+which python3.9
+pwd
+cd /usr/local/bin/
+yum install sqlite-devel -y
+./configure --enable-optimizations --enable-loadable-sqlite-extensions
+make && make altinstall
+cd /vercel/path0/
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+python3.9 manage.py collectstatic --noinput
