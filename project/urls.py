e95c64e243166fbadfdaadc137fe596546c280d5
anurag singh <anurag6569201@gmail.com>
2024-04-11 10:07:25 +0530

    updated timeline page

--- project/urls.py
+++ project/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-
+from project import views
 app_name="project"
 
-urlpatterns=[]
+urlpatterns=[
+    path('time/', views.timeline,name="timeline"),
+]
--- project/views.py
+++ project/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from project.models import TimeLine
 # Create your views here.
+def timeline(request):
+    timeline = TimeLine.objects.all().order_by('-dateAndTime')
+    context={
+        'timeline':timeline,
+    }
+    return render(request,"core/timeline.html",context)
