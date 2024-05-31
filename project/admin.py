93e3efefdfee1301e650045a5097fdd42e778261
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-06-01 01:14:47 +0530

    updated new things

--- project/admin.py
+++ project/admin.py
@@ -4,15 +4,31 @@ from .models import HomeStats,Biography,TimeLine,TechStack,AreaInterest,TechLang
 class ProjectsModClass(admin.ModelAdmin):
     list_display=['projectName']
 
+class ProjectsLanguageClass(admin.ModelAdmin):
+    list_display=['language']
 
+class ProjectsGenereClass(admin.ModelAdmin):
+    list_display=['genereId','genereName']
+
+class ProjectsTimelineClass(admin.ModelAdmin):
+    list_display=['contentStart','contentMid','contentEnd']
+
+class ProjectsTechstackClass(admin.ModelAdmin):
+    list_display=['description']
+
+class ProjectsTechlangClass(admin.ModelAdmin):
+    list_display=['language','techstack']
+
+class ProjectsContactClass(admin.ModelAdmin):
+    list_display=['contact_href']
 
 admin.site.register(HomeStats)
 admin.site.register(AreaInterest)
 admin.site.register(Biography)
-admin.site.register(TimeLine)
-admin.site.register(TechStack)
-admin.site.register(TechLang)
-admin.site.register(Contact)
-admin.site.register(projectLanguage)
-admin.site.register(ProjectsGenere)
+admin.site.register(TimeLine,ProjectsTimelineClass)
+admin.site.register(TechStack,ProjectsTechstackClass)
+admin.site.register(TechLang,ProjectsTechlangClass)
+admin.site.register(Contact,ProjectsContactClass)
+admin.site.register(projectLanguage,ProjectsLanguageClass)
+admin.site.register(ProjectsGenere,ProjectsGenereClass)
 admin.site.register(ProjectsMod,ProjectsModClass)
