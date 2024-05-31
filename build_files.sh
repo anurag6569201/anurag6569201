be4a5383acc1f47444a0c73e07140ba2b732b9fc
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-06-01 01:32:53 +0530

    testing 4

--- build_files.sh
+++ build_files.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 set -o errexit
 
+# Ensure pip is installed
+echo "Ensuring pip is installed..."
+python3.9 -m ensurepip --upgrade
+
 # Install dependencies
 echo "Installing dependencies..."
+python3.9 -m pip install --upgrade pip
 python3.9 -m pip install -r requirements.txt
 
 # Make Migrations
