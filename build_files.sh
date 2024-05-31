4c42d23df9dd05fba9826e2f65aae7476eeef6d3
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-06-01 01:35:33 +0530

    testing 5

--- build_files.sh
+++ build_files.sh
@@ -3,18 +3,18 @@ set -o errexit
 
 # Ensure pip is installed
 echo "Ensuring pip is installed..."
-python3.9 -m ensurepip --upgrade
+python3.10 -m ensurepip --upgrade
 
 # Install dependencies
 echo "Installing dependencies..."
-python3.9 -m pip install --upgrade pip
-python3.9 -m pip install -r requirements.txt
+python3.10 -m pip install --upgrade pip
+python3.10 -m pip install -r requirements.txt
 
 # Make Migrations
 echo "Make Migration..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python3.10 manage.py makemigrations --noinput
+python3.10 manage.py migrate --noinput
 
 # Collect Static
 echo "Collect Static..."
-python3.9 manage.py collectstatic --noinput --clear
+python3.10 manage.py collectstatic --noinput --clear
