16895d15ef6689c74598d049e7e7fd18d05ea704
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-06-01 01:24:48 +0530

    testing 1

--- build_files.sh
+++ build_files.sh
@@ -1,13 +1,8 @@
 set -o errexit
 
-# Build the project
-echo "Building the project..."
-pip install -r requirements.txt
-pip install django==5.0
-
 echo "Make Migration..."
-manage.py makemigrations --noinput
-manage.py migrate --noinput
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
 
 echo "Collect Static..."
-manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput --clear
