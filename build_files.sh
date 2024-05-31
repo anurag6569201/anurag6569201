8ab3597e4a30fdcb9a6a2f9b6e372daa768b29a4
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-06-01 01:26:16 +0530

    testing 2

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,10 @@
 set -o errexit
 
+# Build the project
+echo "Building the project..."
+python3.9 -m pip3 install -r requirements.txt
+python3.9 -m pip3 install django==5.0
+
 echo "Make Migration..."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
