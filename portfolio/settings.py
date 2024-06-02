7cf9f7de0332a674c1fddccc6edd7eb348f23536
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-06-02 09:04:27 +0530

    updated static settings:

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -137,18 +137,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 STATIC_URL = 'static/'
-STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-# renderstorage
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
-
-# Add this setting to ignore missing files warnings (useful for production)
 WHITENOISE_MANIFEST_STRICT = False
 
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
