d654ed625534520502a4ce9ab5680c23c6e8a3cd
anurag singh <140162738+anurag6569201@users.noreply.github.com>
2024-10-20 19:41:22 +0530

    updated images to webp

--- project/forms.py
+++ project/forms.py
@@ -3,4 +3,4 @@ from django import forms
 class ContactForm(forms.Form):
     name=forms.CharField(max_length=255)
     email=forms.EmailField()
-    content=forms.CharField(widget=forms.Textarea)
+    content=forms.CharField(widget=forms.Textarea,rowspan=3)
